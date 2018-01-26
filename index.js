6fe94656e6c34b5bafd81b089989e5eca59001ec
abumalick <abdullah.hilson@gmail.com>
2018-01-26 21:55:41 +0200

    feat: import translations from a json file

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 // inspiration from original plugin and https://github.com/babel/babel/blob/1215db2dd3a6f1c13b2b446f05a2b55791c7a2a6/packages/babel-plugin-transform-es2015-template-literals/src/index.js
+const nodejsPath = require('path');
+const fs = require('fs');
 
 module.exports = function({ types: t }) {
     return {
+        pre() {
+            this.translations = undefined;
+        },
         visitor: {
             TaggedTemplateExpression(path, state) {
                 const translationFunctionName = state.opts.functionName || 't';
                 if (t.isIdentifier(path.node.tag) && path.node.tag.name === translationFunctionName) {
-                    const dictionary = state.opts.dictionary || {};
+                    if (!this.translations) {
+                        if (typeof state.opts.dictionary === 'string') {
+                            try {
+                                const translationsFile = nodejsPath.resolve(process.cwd(), state.opts.dictionary);
+                                console.log(`Reading: ${translationsFile} ...`);
+                                this.translations = JSON.parse(fs.readFileSync(translationsFile, 'utf-8'));
+                                console.log('Successfully imported translations.');
+                            }
+                            catch (err) {
+                                this.translations = {};
+                                console.warn(err.message);
+                            }
+                        }
+                        else {
+                            this.translations = state.opts.dictionary || {};
+                        }
+                    }
                     const { quasi } = path.node;
                     const { expressions, quasis } = quasi;
                     const nodes = [];
                     quasis.forEach((templateElement, index) => {
                         const cooked = templateElement.value.cooked;
                         if (cooked) {
-                            nodes.push(t.stringLiteral(dictionary[cooked] || cooked));
+                            nodes.push(t.stringLiteral(this.translations[cooked] || cooked));
                         }
                         if (index < expressions.length) {
                             const expr = expressions[index];
