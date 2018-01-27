b8c813872e55aef311bd423dc7fcf2e5990b69f0
abumalick <abdullah.hilson@gmail.com>
2018-01-27 10:09:02 +0200

    fix(tokens): support tokens in translation

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 const nodejsPath = require('path');
 const fs = require('fs');
 
+const escape = (s) => {
+    return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+};
+
 module.exports = function({ types: t }) {
     return {
         pre() {
@@ -11,13 +15,13 @@ module.exports = function({ types: t }) {
             TaggedTemplateExpression(path, state) {
                 const translationTagName = state.opts.tagName || 't';
                 if (t.isIdentifier(path.node.tag) && path.node.tag.name === translationTagName) {
+                    // import translations
                     if (!this.translations) {
                         if (typeof state.opts.translations === 'string') {
                             try {
                                 const translationsFile = nodejsPath.resolve(process.cwd(), state.opts.translations);
                                 console.log(`Reading: ${translationsFile} ...`);
                                 this.translations = JSON.parse(fs.readFileSync(translationsFile, 'utf-8'));
-                                console.log('Successfully imported translations.');
                             }
                             catch (err) {
                                 this.translations = {};
@@ -28,19 +32,46 @@ module.exports = function({ types: t }) {
                             this.translations = state.opts.translations || {};
                         }
                     }
+
                     const { quasi } = path.node;
                     const { expressions, quasis } = quasi;
-                    const nodes = [];
+                    const preToken = state.opts.preToken || '#';
+                    const postToken = state.opts.postToken || '#';
+
+                    // create the original string with tokens inside (eg: 'Hello #1#')
+                    let baseString = '';
                     quasis.forEach((templateElement, index) => {
                         const cooked = templateElement.value.cooked;
                         if (cooked) {
-                            nodes.push(t.stringLiteral(this.translations[cooked] || cooked));
+                            baseString += cooked;
                         }
                         if (index < expressions.length) {
-                            const expr = expressions[index];
-                            if (!t.isStringLiteral(expr, { value: '' })) {
-                                nodes.push(expr);
+                            baseString += `${preToken}${index + 1}${postToken}`;
+                        }
+                    });
+
+                    // get translated value from translations
+                    const translatedString = this.translations[baseString] || baseString;
+
+                    // split the translation into strings and variables (eg: ['Salut ', '#1#'])
+                    const tokenReg = new RegExp(`(${escape(preToken)}\\d{1,3}${escape(postToken)})`, 'g');
+                    const tokenNumberReg = new RegExp(`${escape(preToken)}(\\d{1,3})${escape(postToken)}`);
+                    const translationItems = translatedString.split(tokenReg);
+
+                    // create an array of nodes for replacement
+                    const nodes = [];
+                    translationItems.filter((item) => item !== '').forEach((item, index) => {
+                        if (tokenNumberReg.test(item)) {
+                            const number = tokenNumberReg.exec(item);
+                            if (number && number[1] && number[1] <= expressions.length) {
+                                const expr = expressions[number[1] - 1];
+                                if (!t.isStringLiteral(expr, { value: '' })) {
+                                    nodes.push(expr);
+                                }
                             }
+                        }
+                        else {
+                            nodes.push(t.stringLiteral(item));
                         }
                     });
                     let root = nodes[0];
